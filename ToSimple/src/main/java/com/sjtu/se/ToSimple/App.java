f5eb027f1b6339403d17334036fe1447cf8d86e0
Junyi Liu <1072207255@qq.com>
2017-06-26 11:22:48 +0800

    initial maven project

---
+++ ToSimple/src/main/java/com/sjtu/se/ToSimple/App.java
@@ -0,0 +1,13 @@
+package com.sjtu.se.ToSimple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
