bfaf387b3a3765c008a7714b7e8979fc6ac207a4
Junyi Liu <1072207255@qq.com>
2017-06-30 15:22:04 +0800

    RSA

--- ToSimple/src/main/java/action/UserAction.java
+++ ToSimple/src/main/java/action/UserAction.java
@@ -1,10 +1,21 @@
 package action;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts2.ServletActionContext;
+
+import model.RSAUtils;
 import model.User;
+import net.sf.json.JSONObject;
 import service.UserService;
+
 
 public class UserAction extends BaseAction{
 	private static final long serialVersionUID = 1L;
@@ -12,7 +23,8 @@ public class UserAction extends BaseAction{
 	public void setUserService(UserService userService) {
 		this.userService = userService;
 	}
-
+	
+	private String passwordSECURE;
 	private int id;
 	private String userName;
 	private String password;
@@ -25,6 +37,36 @@ public class UserAction extends BaseAction{
 		users = userService.getAllUsers();
 		return SUCCESS;
 	}
+	
+	public String fetchRSA() throws Exception{
+		HashMap<String, Object> map = RSAUtils.getKeys();
+        RSAPublicKey publicKey = (RSAPublicKey) map.get("public");
+        RSAPrivateKey privateKey = (RSAPrivateKey) map.get("private");
+        HttpSession session =session();
+        session.setAttribute("privateKey", privateKey);
+        
+        String modulus = publicKey.getModulus().toString();
+        String publicExponent = publicKey.getPublicExponent().toString();
+        String privateExponent = privateKey.getPrivateExponent().toString();
+        //明文
+        
+        JSONObject result = new JSONObject();
+		result.put("modulus", modulus);
+		result.put("publicExponent", publicExponent);
+		ServletActionContext.getResponse().getWriter().print(result);
+		return null;
+		
+	}
+	
+	public String login() throws Exception{
+		HttpSession session =session();
+		RSAPrivateKey privateKey = (RSAPrivateKey) session.getAttribute("privateKey");
+		String passwordInput=RSAUtils.decryptByPrivateKey(passwordSECURE, privateKey);
+		return null;
+	}
+	
+	
+	//helper
 
 	public int getId() {
 		return id;
