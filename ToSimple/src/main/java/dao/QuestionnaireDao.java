517a795d292851705be91cdcec4a2745d1647eb8
linshengli <1250979088@qq.com>
2017-07-06 11:37:03 +0800

    增加search 功能：搜索功能

--- ToSimple/src/main/java/dao/QuestionnaireDao.java
+++ ToSimple/src/main/java/dao/QuestionnaireDao.java
@@ -5,6 +5,8 @@ import com.mongodb.DBObject;
 import model.Questionnaire;
 import net.sf.json.JSONObject;
 
+import java.util.List;
+
 public interface QuestionnaireDao {
     public String save(Questionnaire questionnaire);
 
@@ -21,6 +23,40 @@ public interface QuestionnaireDao {
     public Questionnaire findQuestionnaireById(String id);
 
     public Questionnaire findQuestionnaireByUser(int id);
+
+    /**
+     * search questionnaires by name.(fuzzy search)
+     * @param name the name
+     * @param size the number of questionnaire not allowed bigger than 30.
+     * @return  the list of questionnaires
+     */
+    public List<Questionnaire> searchQuestionnaireByName(String name,Integer size);
+
+    /**
+     * search questionnaires by name.(fuzzy search)
+     * @param name the name
+     * @return  the list of questionnaires
+     */
+    public List<Questionnaire> searchQuestionnaireByName(String name);
+
+
+
+    /**
+     * find questionnaires by status
+     * @param status the status of the questionnaire
+     * @return the list of questionnaire
+     */
+    public List<Questionnaire> findQuestionnaireByStatus(Integer status);
+
+//    /**
+//     * find questionnaires by status
+//     * @param status the status of the questionnaire
+//     * @param size the number of questionnaire not allowed bigger than 30.
+//     * @return the list of questionnaire
+//     */
+//    public List<Questionnaire> findQuestionnaireByStatus(Integer status,Integer size);
+
+
 
 //	public User getQuestionnaireById(int id);
 //
