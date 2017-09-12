18bb21cda9a05671bd7153a7d5585603b0b8b882
Jimmy Wang <wangziming1022@foxmail.com>
2017-09-12 21:36:26 +0800

    修改主页

--- frontend/out/server.js
+++ frontend/out/server.js
@@ -101,7 +101,7 @@ http.createServer(function (request, response) {
                 var resContentType = mime[extname] || "text/plain";
     
                 response.writeHead(200, { "Content-type": resContentType });
-                if (extname == "woff" || extname == "woff2") {
+                if (extname == "woff" || extname == "woff2" || extname == "jpg") {
                     fs.readFile(fpath, "binary", function (err, data) {
                         var rsp = data;
                         response.end(rsp, "binary");
